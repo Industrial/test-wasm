77dcebd9ed0f426ab3dc78098bf4ee585add5760
Tom Wieland <tom.wieland@gmail.com>
2023-05-11 15:42:02 +0200

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    flake-compat,
+  } @ inputs: let
+    inherit (nixpkgs) lib;
+    inherit (lib) recursiveUpdate;
+    inherit (flake-utils.lib) eachDefaultSystem defaultSystems;
+
+    nixpkgsFor = lib.genAttrs defaultSystems (system:
+      import nixpkgs {
+        inherit system;
+      });
+  in (eachDefaultSystem (
+    system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          bashInteractive
+        ];
+        buildInputs = with pkgs; [
+          git
+          nodejs-19_x
+          nodejs-19_x.pkgs.eslint
+          nodejs-19_x.pkgs.pnpm
+          nodejs-19_x.pkgs.typescript
+          openssl
+        ];
+      };
+    }
+  ));
+}
