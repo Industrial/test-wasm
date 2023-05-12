f9ce852bc4e052f0ba5873ad2cb44bd9b7545d4c
Tom Wieland <tom.wieland@gmail.com>
2023-05-12 16:50:15 +0200

    feat: small example using cabal package

--- apps/web-haskell/app/Main.hs
+++ apps/web-haskell/app/Main.hs
@@ -1,11 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Main where
-
-import Web.Scotty
-
+import qualified Data.ByteString.Char8 as C8
+import Data.Char (toUpper)
 
 main :: IO ()
-main = scotty 3030 $
-    get "/:word" $ do
-        beam <- param "word"
-        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+main = do
+  let inputString = "Hello, ByteString!"
+  let byteString = C8.pack inputString
+
+  -- Display the original ByteString
+  putStrLn "Original ByteString:"
+  print byteString
+
+  -- Convert ByteString to String, perform operations, and convert back to ByteString
+  let modifiedString = map toUpper (C8.unpack byteString)
+  let modifiedByteString = C8.pack modifiedString
+
+  -- Display the modified ByteString
+  putStrLn "Modified ByteString:"
+  print modifiedByteString
